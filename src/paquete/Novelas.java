6932dbe35f3d1fcabc51f428c3459f48bdfc00d7
Eduardo1602 <edusan1602@gmail.com>
2021-04-13 13:13:57 -0600

    Create Novelas.java

---
+++ src/paquete/Novelas.java
@@ -0,0 +1,65 @@
+
+package paquete;
+
+public class Novelas {
+    
+    private String nombre;
+    private String Lenguaje;
+    private String Descripcion;
+    private String Comentario;
+    private Categoria Categoria;
+    
+    public Novelas(){
+        
+    }
+
+    public Novelas(String nombre, String Lenguaje, String Descripcion, String Comentario, Categoria Categoria) {
+        this.nombre = nombre;
+        this.Lenguaje = Lenguaje;
+        this.Descripcion = Descripcion;
+        this.Comentario = Comentario;
+        this.Categoria = Categoria;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getLenguaje() {
+        return Lenguaje;
+    }
+
+    public void setLenguaje(String Lenguaje) {
+        this.Lenguaje = Lenguaje;
+    }
+
+    public String getDescripcion() {
+        return Descripcion;
+    }
+
+    public void setDescripcion(String Descripcion) {
+        this.Descripcion = Descripcion;
+    }
+
+    public String getComentario() {
+        return Comentario;
+    }
+
+    public void setComentario(String Comentario) {
+        this.Comentario = Comentario;
+    }
+
+    public Categoria getCategoria() {
+        return Categoria;
+    }
+
+    public void setCategoria(Categoria Categoria) {
+        this.Categoria = Categoria;
+    }
+    
+    
+}
